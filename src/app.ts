6936463fe944c967f2c35c2aa2ceed436a917b70
黑白 <richardfox@foxmail.com>
2020-11-09 17:26:43 +0800

    router

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,8 @@
 import * as Koa from 'koa'
-import * as Router from 'koa-router'
+import router from './routes'
 
 const app = new Koa()
-const router = new Router()
-
-router.get('/', async ctx => {
-  ctx.body = 'hello world!'
-})
 
 app.use(router.routes())
   .use(router.allowedMethods())
   .listen(3000)
-
-export default app
